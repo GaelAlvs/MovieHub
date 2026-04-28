fdcb31e8d64470dbad87cb037440c218c2b79668
Gabriel <gabriel.alvs19@gmail.com>
2026-04-28 14:30:40 -0300

    Criação do footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { Hero } from './components/hero/hero';
+import { Recommendations } from './components/recommendations/recommendations';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Hero],
+  imports: [RouterOutlet, Navbar, Hero, Recommendations, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
